4988850028e4dfade2cdb1229e8b79743992b957
Jakub Oriniak <243036@vutbr.cz>
2022-05-03 17:15:33 +0200

    1

--- fibo.py
+++ fibo.py
@@ -1,6 +1,18 @@
 
+def recursive_nth_fibo(konecnaPozice):
+    if konecnaPozice == 0:
+        return 0
+    elif konecnaPozice==1:
+        return 1
+    else:
+        return recursive_nth_fibo(konecnaPozice-1) + recursive_nth_fibo(konecnaPozice-2)
+
+
+
 def main():
-    pass
+
+    cislo = int(input("zadej pozici konecneho prvku"))
+    print(recursive_nth_fibo(cislo))
 
 
 if __name__ == "__main__":
